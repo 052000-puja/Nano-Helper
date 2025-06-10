a8741498f3653a6646a5077210c2faf39e569976
Puja Rani <78751475+052000-puja@users.noreply.github.com>
2025-06-10 14:27:49 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function UserContext({children}) {
-  const serverUrl="http://localhost:8000"
+  const serverUrl="https://nanohelper-backend.onrender.com"
   const [userData, setUserData] = useState(null)
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
