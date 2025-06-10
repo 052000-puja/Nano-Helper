c6ac639974730a85f5825ee927abfefaa00d1c29
Puja Rani <78751475+052000-puja@users.noreply.github.com>
2025-06-10 15:05:57 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -28,8 +28,8 @@ export const signUp= async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            secure: false,
-            sameSite: "strict"
+            secure: true,
+            sameSite: "None"
         })
 
         return res.status(201).json(user)
@@ -59,8 +59,8 @@ export const logIn = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-            secure: false,
-            sameSite: "strict"
+            secure: true,
+            sameSite: "None"
         })
 
         return res.status(200).json(user)
